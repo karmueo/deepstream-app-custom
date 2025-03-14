2d14d82f8a0d40782f2256c79613aa375df304e8
karmueo <karmueo@163.com>
2025-03-14 15:36:26 +0800

    更新ONNX到TensorRT转换脚本，调整输入输出路径和形状，添加Triton主推理配置文件，更新DeepStream应用配置以支持Triton推理

--- convert2trt.sh
+++ convert2trt.sh
@@ -3,22 +3,20 @@
 # trtexec_convert.sh - 转换ONNX到TensorRT engine的脚本
 
 # 输入参数
-ONNX_PATH="triton_model/Secondary_Classify/1/efficientnet_110_with_softmax.onnx"  # 输入ONNX文件路径
-ENGINE_PATH="efficientnet_fp16.engine"           # 输出engine文件路径
+ONNX_PATH="triton_model/Primary_Detect/1/yolov11_ir_drones_p2_single_target_end2end.onnx"  # 输入ONNX文件路径
+ENGINE_PATH="triton_model/Primary_Detect/1/yolov11_ir_drones_p2_single_target_end2end.engine"           # 输出engine文件路径
 PRECISION="fp32"                                 # 精度 (fp32/fp16/int8)
-MAX_BATCH=8                                      # 最大batch_size
+MAX_BATCH=4                                      # 最大batch_size
 
 # 动态形状配置（min/opt/max batch）
 MIN_BATCH=1
-OPT_BATCH=4
+OPT_BATCH=1
 
 # 使用trtexec转换
 /usr/src/tensorrt/bin/trtexec \
   --onnx=$ONNX_PATH \
   --saveEngine=$ENGINE_PATH \
-  --minShapes=input:${MIN_BATCH}x3x224x224 \
-  --optShapes=input:${OPT_BATCH}x3x224x224 \
-  --maxShapes=input:${MAX_BATCH}x3x224x224 \
-  --inputIOFormats=fp32:chw \
-  --outputIOFormats=fp32:chw \
+  --minShapes=images:${MIN_BATCH}x3x640x640 \
+  --optShapes=images:${OPT_BATCH}x3x640x640 \
+  --maxShapes=images:${MAX_BATCH}x3x640x640 \
   --verbose
