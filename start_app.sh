825dd6394a6d919727474a071fc291e728d4d1b6
karmueo <karmueo@163.com>
2025-03-18 19:16:25 +0800

    更新DeepStream应用配置，修改RTSP源地址和重连尝试次数，调整启动脚本路径

--- start_app.sh
+++ start_app.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-src/deepstream-app/deepstream-app -c src/deepstream-app/configs/deepstream_app_config.txt
+/opt/nvidia/deepstream/deepstream/bin/deepstream-app -c /workspace/deepstream-app-custom/src/deepstream-app/configs/deepstream_app_config.txt
