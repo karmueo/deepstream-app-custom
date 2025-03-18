6de706bb7edb2ab664717fe4b1bb2e2b0b9ca322
karmueo <karmueo@163.com>
2025-03-18 20:03:42 +0800

    添加启动Triton推理服务器的脚本，更新DeepStream应用启动脚本以支持Triton推理

--- start_service.sh
+++ start_service.sh
@@ -1,7 +1,11 @@
-#/bin/bash
+#!/bin/bash
 # Start Triton Inference Server with the converted models
 /opt/tritonserver/bin/tritonserver \
     --model-repository=./triton_model \
     --disable-auto-complete-config \
     --log-verbose=0 \
     > triton.log 2>&1 &
+
+sleep 2
+
+/opt/nvidia/deepstream/deepstream/bin/deepstream-app -c /workspace/deepstream-app-custom/src/deepstream-app/configs/deepstream_app_config.txt
