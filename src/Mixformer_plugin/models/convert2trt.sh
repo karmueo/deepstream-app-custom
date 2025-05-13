2fa15d25e1bd49c672e34f4513ea2126169e73fc
karmueo <karmueo@163.com>
2025-05-13 09:50:42 +0000

    ostrack模型可以支持fp16而不掉精度

--- src/Mixformer_plugin/models/convert2trt.sh
+++ src/Mixformer_plugin/models/convert2trt.sh
@@ -2,8 +2,9 @@
 # trtexec_convert.sh - 转换ONNX到TensorRT engine的脚本
 # 跟踪配置
 ONNX_PATH="ostrack-384-ep300-ce.onnx"  # 输入ONNX文件路径
-ENGINE_PATH="ostrack-384-ep300-ce_fp32.engine"           # 输出ENGINE文件路径
+ENGINE_PATH="ostrack-384-ep300-ce_fp16.engine"           # 输出ENGINE文件路径
 /usr/src/tensorrt/bin/trtexec \
   --onnx=$ONNX_PATH \
   --saveEngine=$ENGINE_PATH \
-  --verbose # --precisionConstraints=obey --fp16
+  --verbose \
+  --fp16
