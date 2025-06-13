8a44a74e035263e0178218db119ecda481453748
karmueo <karmueo@163.com>
2025-06-13 08:14:07 +0000

    在start_app.sh中添加nohup以后台运行deepstream-app，确保应用在终端关闭后继续运行

--- start_app.sh
+++ start_app.sh
@@ -3,4 +3,4 @@
 export GST_DEBUG_NO_COLOR=1
 export GST_DEBUG_FILE=./app.log
 export GST_DEBUG=4
-src/deepstream-app/deepstream-app -c src/deepstream-app/configs/deepstream_app_config.txt
+nohup src/deepstream-app/deepstream-app -c src/deepstream-app/configs/deepstream_app_config.txt &
