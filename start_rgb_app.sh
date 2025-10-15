2b699f8fde123cf04cc57422806979445647aec4
karmueo <karmueo@163.com>
2025-10-15 15:19:32 +0800

    修复启动脚本中的路径，确保正确调用深度流应用程序

--- start_rgb_app.sh
+++ start_rgb_app.sh
@@ -4,4 +4,4 @@ export GST_DEBUG_NO_COLOR=1
 export GST_DEBUG_FILE=./app_rgb.log
 export GST_DEBUG=4
 # nohup src/deepstream-app/deepstream-app -c src/deepstream-app/configs/ir_app_config.txt &
-src/deepstream-app/deepstream-app -c src/deepstream-app/configs/rgb_app_config.txt
+./build/deepstream-app -c src/deepstream-app/configs/rgb_app_config.txt
